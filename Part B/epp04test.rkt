8c77e7f73593111a139c57f08e0abc14a3f14d77
Pieter van Wyk <superfluidbcsbec@ezweb.ne.jp>
2020-10-09 17:10:43 +0900

    Add files via upload

---
+++ Part B/epp04test.rkt
@@ -0,0 +1,21 @@
+#lang racket
+(provide (all-defined-out))
+(require rackunit)
+(require "epp_week1.rkt")
+
+
+(define tests
+  (test-suite
+   "unit tests for extra practice problems"
+
+   ; unit tests for problem 1
+   (check-equal? (add-lists '(1 2 3) '(1 2 3)) '(2 4 6) "add-list test 1")
+   ;(check-equal? (add-lists (list 1 2 3) (list 1 2 3)) (list 2 4 6) "add-list test 1")
+   
+   
+   
+   ))
+
+(require rackunit/text-ui)
+;; runs the test
+(run-tests tests)
