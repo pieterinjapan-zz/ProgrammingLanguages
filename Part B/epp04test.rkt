23f05dc1ef00aa76d9f7cc9b21be29cdc3299ce3
Pieter van Wyk <superfluidbcsbec@ezweb.ne.jp>
2020-10-09 17:12:20 +0900

    Update epp04test.rkt

--- Part B/epp04test.rkt
+++ Part B/epp04test.rkt
@@ -1,6 +1,7 @@
 #lang racket
 (provide (all-defined-out))
 (require rackunit)
+(require rackunit/text-ui)
 (require "epp04.rkt")
 
 (define tests
@@ -11,8 +12,6 @@
    (check-equal? (add-lists '(1 2 3) '(1 2 3)) '(2 4 6) "add-list test 1")
       
    ))
-
-(require rackunit/text-ui)
 
 ;; runs the test
 (run-tests tests)
