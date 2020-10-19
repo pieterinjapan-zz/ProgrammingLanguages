e591c37aee82ab2686e6b4cb117bf7fbbaf6fd7b
Pieter van Wyk <superfluidbcsbec@gmail.com>
2020-10-20 07:38:14 +0900

    Update epp04.rkt

--- Part B/epp04.rkt
+++ Part B/epp04.rkt
@@ -1,10 +1,9 @@
 ; Author  : Pieter van Wyk
 ; Created : 2020-10-03
-; Updated : 2020-10-15
+; Updated : 2020-10-
 ;
 ; Solutions to the extra practice problems of week 1 of part B 
 ;
-
 #lang racket
 (provide (all-defined-out))
 
@@ -30,9 +29,10 @@
 ; Define a stream facts, the stream of factorials of the natural
 ; numbers starting at 0.
 
-(define facts
-  (letrec ([aux (λ(x y) (λ()(cons x (aux (* x y) (+ y 1)))))])
-      (aux 1 1)))
+; helper function for testing streams (returns the first n elements of a stream)
+(define (stream-to-list stream n)
+  (cond [(= n 0) null]
+        [#t (cons (car (stream)) (stream-to-list (cdr (stream)) (- n 1)))]))
 
 ; helper functions for testing facts
 (define nats
@@ -42,20 +42,26 @@
 (define (fact n)
   (cond [(zero? n) 1]
         [#t (* n (fact (- n 1)))]))
-        
-        
+
+; main
+(define facts
+  (letrec ([aux (λ(x y) (λ()(cons x (aux (* x y) (+ y 1)))))])
+      (aux 1 1)))
+
+
 ; --- problem 2 (b) ---
 ; Define a stream fibonacci, the first element of which is 0, the second one is 1, and each
 ; successive element is the sum of two immediately preceding elements.
 
-(define fibonacchi
-  (letrec ([aux (λ(x y) (λ()(cons x (aux y (+ x y)))))])
-      (aux 0 1)))
-
 ; helper functions for testing fibonacchi
 (define (fibo n)
   (cond [(< n 2) n]
-        [#t  (+ (fibo (- n 1)) (fibo (- n 2)))]))        
+        [#t  (+ (fibo (- n 1)) (fibo (- n 2)))]))
+
+; main
+(define fibonacchi
+  (letrec ([aux (λ(x y) (λ()(cons x (aux y (+ x y)))))])
+      (aux 0 1)))
 
 
 ; --- problem 3 ---
@@ -63,15 +69,37 @@
 ; values of s in succession until f evaluates to #f. The values after applying f are returned
 ; as a list.
 
-(define (stream-until f s)
-  (cond [(false? (f (car (s)))) null]
-        [#t (cons (f (car (s))) (stream-until f (cdr (s))))]))
-
 ; helper for testing (takes integer input function f and integer n-max
 ; and returns a function that evaluates to #f if the input is above
 ; n-max, otherwise applying f)
 (define (f-cond f n-max)
   (λ(n) (cond [(>= n n-max) #f]
               [#t (f n)])))
+
+; main
+(define (stream-until f s)
+  (cond [(false? (f (car (s)))) null]
+        [#t (cons (f (car (s))) (stream-until f (cdr (s))))]))
+
+
+; --- problem 4 ---
+; Write a function stream-map that takes a function f and a stream s, and returns a new stream
+; whose values are the result of applying f to the values produced by s.
+
+(define (stream-map f s) (λ() (cons (f (car(s))) (stream-map f (cdr (s))))))
+
+
+; --- problem 5 ---
+; Write a function stream-zip that takes in two streams s1 and s2 and returns a stream that produces
+; the pairs that result from the other two streams (so the first value for the result stream will be
+; the pair of the first value of s1 and the first value of s2).
+
+; helper : zip function
+(define (zip xs ys)
+  (cond [(or (null? xs) (null? ys)) null]
+        [#t (cons (list (car xs) (car ys)) (zip (cdr xs) (cdr ys)))]))
+
+; main
+(define (stream-zip s1 s2) (λ() (cons (list (car(s1)) (car(s2))) (stream-zip (cdr(s1)) (cdr(s2))))))
               
 ; END
